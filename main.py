593bfa8509731b78d1d89ee0fec783b646e437d6
samgfk <samgfk@yahoo.com>
2025-07-23 00:23:54 -0400

    Added get_books_text function and main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+
+def get_books_text(file_path):
+    #A with block can be used to open a file
+
+    with open(file_path) as f:
+        #use the .read() method to read the contents of a file into a string
+
+        book_text = f.read()
+        return print(book_text)
+
+def main():
+    get_books_text("./books/frankenstein.txt")
+    return 0
+
+
+main()
+
+
